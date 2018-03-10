db7feeb24cfa77489be02adf2179d2b4df4b4e15
Domenik Müller <dmin7@digitalites.org>
2018-03-10 11:16:28 +0100

    init ev test icosoc project

---
+++ src/icotools/icosoc/examples/ev_test_01/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "icosoc.h"
+
+int main()
+{
+	
+	// main loop
+	for (uint8_t i = 0;; i++)
+	{
+		icosoc_leds(i);
+		
+		// debug print
+		printf("[%02x] Hello World!\n", i);
+
+		// sleep for a bit ..
+		for (int i = 0; i < 100000; i++)
+			asm volatile ("");
+
+	}
+}
+
