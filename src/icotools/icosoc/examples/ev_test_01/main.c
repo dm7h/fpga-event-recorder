200a5214c233973a3711e8618daa0366816cba7f
Domenik Müller <dmin7@digitalites.org>
2018-03-29 12:31:01 +0200

    raspif & SPI tests

--- src/icotools/icosoc/examples/ev_test_01/main.c
+++ src/icotools/icosoc/examples/ev_test_01/main.c
@@ -2,6 +2,34 @@
 #include <stdint.h>
 #include "icosoc.h"
 
+
+static int console_getc()
+{
+	while (1) {
+		int c = *(volatile uint32_t*)0x30000000;
+		if (c >= 0) return c;
+	}
+}
+
+
+static void spiflash_begin()
+{
+	*(volatile uint32_t*)0x20000004 &= ~8;
+}
+
+static void spiflash_end()
+{
+	*(volatile uint32_t*)0x20000004 |= 8;
+}
+
+static uint8_t spiflash_xfer(uint8_t value)
+{
+	*(volatile uint32_t*)0x20000008 = value;
+	return *(volatile uint32_t*)0x20000008;
+}
+
+
+
 int main()
 {
 	
@@ -9,12 +37,17 @@ int main()
 	for (uint8_t i = 0;; i++)
 	{
 		icosoc_leds(i);
-		
 		// debug print
-		printf("[%02x] 'elo there!\n", i);
-
+		int t = 't';
+		printf("%c, elo there\n", t);
+		/*
+		spiflash_end();
+		spiflash_begin();
+		spiflash_xfer(0x17);
+		spiflash_end();
+		*/
 		// sleep for a bit ..
-		for (int i = 0; i < 100000; i++)
+		for (int i = 0; i < 1000000; i++)
 			asm volatile ("");
 
 	}
